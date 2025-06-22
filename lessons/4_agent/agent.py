aa577f2961d03b1c3ef9b7b2637c80b27a32ac66
ruslanivanov <ruslan4432013@gmail.com>
2025-06-22 22:12:51 +0300

    update comments in lesson 4 agent

--- lessons/4_agent/agent.py
+++ lessons/4_agent/agent.py
@@ -68,8 +68,8 @@ builder.add_node("tools", tools_node)
 builder.add_edge(START, "assistant")
 builder.add_conditional_edges(
     "assistant",
-    # If the latest message (result) from assistant is a tool call -> tools_condition routes to tools
-    # If the latest message (result) from assistant is a not a tool call -> tools_condition routes to END
+    # Если последнее сообщение - вызов инструмента → переход к tools
+    # Если не вызов инструмента → переход к END
     tools_condition
 )
 builder.add_edge("tools", "assistant")  # Ключевое ребро цикла
