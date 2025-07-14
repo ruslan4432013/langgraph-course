be763c442b53f1b29012a844e38a68410e55f731
ruslanivanov <ruslan4432013@gmail.com>
2025-07-14 21:18:25 +0300

    add stream lesson

---
+++ lessons/module_3/1_stream/update_stream.py
@@ -0,0 +1,17 @@
+from langchain_core.messages import HumanMessage
+
+from agent import graph
+# Создаем конфигурацию потока
+config = {"configurable": {"thread_id": "1"}}
+
+# # Начинаем диалог
+# for chunk in graph.stream({"messages": [HumanMessage(content="Привет! Я Ланс")]}, config, stream_mode="updates"):
+#     print(chunk)
+
+
+# Начинаем диалог
+for chunk in graph.stream({"messages": [HumanMessage(content="Привет! Я Ланс")]}, config, stream_mode="updates"):
+    for key, value in chunk.items():
+        if 'messages' in value:
+            print(key)
+            value["messages"].pretty_print()
