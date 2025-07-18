cb825d97a885bab28a148cfb4b716ae851e11d50
ruslanivanov <ruslan4432013@gmail.com>
2025-07-18 15:36:04 +0300

    add human feedback with graph-based state editing lesson and update langgraph configuration

---
+++ lessons/module_3/3_editing_state_and_human_feedback/editing_state_and_human_feedback.py
@@ -0,0 +1,46 @@
+from langchain_core.messages import HumanMessage
+
+from react import graph
+
+# Входные данные
+initial_input = {"messages": "Умножь 2 и 3"}
+
+# Поток
+thread = {"configurable": {"thread_id": "1"}}
+
+# Запуск графа до первого прерывания
+print('Interrupt[START]')
+for event in graph.stream(initial_input, thread, stream_mode="values"):
+    print(event['messages'][-1])
+
+state = graph.get_state(thread)
+print(state)
+
+print('Interrupt[END]')
+# Теперь мы можем напрямую применить обновление состояния. Помните, что обновления ключа `messages` будут использовать редьюсер `add_messages`:
+# * Если мы хотим перезаписать существующее сообщение, мы можем указать `id` сообщения.
+# * Если мы просто хотим добавить к нашему списку сообщений, то мы можем передать сообщение без указания `id`, как показано ниже.
+graph.update_state(
+    thread,
+    {"messages": [HumanMessage(content="Нет, на самом деле умножь 3 и 3!")]},
+)
+#
+# Посмотрим
+# Мы вызвали `update_state` с новым сообщением. Редьюсер `add_messages` добавляет его к нашему ключу состояния `messages`.
+new_state = graph.get_state(thread).values
+print('new_state[START]')
+for m in new_state['messages']:
+    print(m)
+print('new_state[END]')
+#
+# state = graph.get_state(thread)
+# print(state.next)
+#
+# Теперь продолжим работу с нашим агентом, просто передав `None` и позволив ему продолжить с текущего состояния.
+# Мы выдаем текущее состояние, а затем переходим к выполнению оставшихся узлов.
+for event in graph.stream(None, thread, stream_mode="values"):
+    print(event['messages'][-1])
+
+# Теперь мы вернулись к `assistant`, у которого есть наша `точка остановки`. Мы снова можем передать `None` для продолжения.
+for event in graph.stream(None, thread, stream_mode="values"):
+    print(event['messages'][-1])
---
+++ lessons/module_3/3_editing_state_and_human_feedback/react.py
@@ -0,0 +1,78 @@
+from langchain_core.messages import SystemMessage
+from langchain_deepseek import ChatDeepSeek
+from langgraph.checkpoint.memory import MemorySaver
+from langgraph.graph import MessagesState
+from langgraph.graph import START, StateGraph
+from langgraph.prebuilt import tools_condition, ToolNode
+
+
+def multiply(a: int, b: int) -> int:
+    """Умножить a и b.
+
+    Аргументы:
+        a: первое целое число
+        b: второе целое число
+    """
+    return a * b
+
+
+# Это будет инструментом
+def add(a: int, b: int) -> int:
+    """Сложить a и b.
+
+    Аргументы:
+        a: первое целое число
+        b: второе целое число
+    """
+    return a + b
+
+
+def divide(a: int, b: int) -> float:
+    """Разделить a на b.
+
+    Аргументы:
+        a: первое целое число
+        b: второе целое число
+    """
+    return a / b
+
+
+tools = [add, multiply, divide]
+
+llm = ChatDeepSeek(
+    model='deepseek-chat',
+    temperature=0.1,
+    max_retries=2,
+    api_base="https://api.proxyapi.ru/deepseek"
+)
+
+llm_with_tools = llm.bind_tools(tools)
+
+# Системное сообщение
+sys_msg = SystemMessage(content="Вы полезный помощник, выполняющий арифметические операции над набором входных данных.")
+
+
+# Узел
+def assistant(state: MessagesState):
+    return {"messages": [llm_with_tools.invoke([sys_msg] + state["messages"])]}
+
+
+# Построитель графа
+builder = StateGraph(MessagesState)
+
+# Определение узлов: они выполняют работу
+builder.add_node("assistant", assistant)
+builder.add_node("tools", ToolNode(tools))
+
+# Определение ребер: они определяют поток управления
+builder.add_edge(START, "assistant")
+builder.add_conditional_edges(
+    "assistant",
+    # Если последнее сообщение (результат) от assistant - вызов инструмента -> tools_condition ведет к tools
+    # Если последнее сообщение (результат) от assistant - не вызов инструмента -> tools_condition ведет к END
+    tools_condition,
+)
+builder.add_edge("tools", "assistant")
+
+memory = MemorySaver()
+graph = builder.compile(interrupt_before=["assistant"], checkpointer=memory)
