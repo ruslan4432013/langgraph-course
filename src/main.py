8357fbfaf7fd69b6e949109a82644b3c2c1d1d74
ruslanivanov <ruslan4432013@gmail.com>
2026-01-11 16:25:39 +0800

    migrate lessons to src directory and refactor project structure

--- src/main.py
+++ src/main.py
@@ -1,24 +1,27 @@
 import json
-from pyexpat.errors import messages
 from typing import Literal
 
 from langchain_core.messages import ToolMessage
 from langchain_core.tools import tool
-from langchain_deepseek import ChatDeepSeek
+from langchain_openai import ChatOpenAI
 from langgraph.constants import END, START
 from langgraph.graph import MessagesState, StateGraph
-from langgraph.prebuilt import ToolNode
+
+from src.settings import settings
+
 
 @tool
 def multiply(a: int, b: int) -> int:
     """Multiply two numbers a and b"""
     return a * b
 
-llm = ChatDeepSeek(
-    model='deepseek-chat',
+
+llm = ChatOpenAI(
+    model="gpt-5.2",
+    api_key=settings.OPENAI_API_KEY,
     temperature=0.1,
     max_retries=2,
-    api_base="https://api.proxyapi.ru/deepseek"  # Необходимо, для работы модели через ProxyApi
+    base_url="https://api.proxyapi.ru/openai/v1"
 )
 
 llm_with_tools = llm.bind_tools([multiply])
@@ -39,7 +42,8 @@ class BasicToolNode:
 
         for tool_call in last_message.tool_calls:
             tool_result = self.tool_by_name[tool_call['name']].invoke(tool_call['args'])
-            outputs.append(ToolMessage(content=json.dumps(tool_result), name=tool_call['name'], tool_call_id=tool_call['id']))
+            outputs.append(
+                ToolMessage(content=json.dumps(tool_result), name=tool_call['name'], tool_call_id=tool_call['id']))
 
         return {'messages': outputs}
 
@@ -47,8 +51,10 @@ class BasicToolNode:
 class State(MessagesState):
     ...
 
+
 def tool_calling_llm(state):
     return {'messages': llm_with_tools.invoke(state['messages'])}
+
 
 def tools_condition(state: State) -> Literal['tools', END]:
     last_message = state.get('messages', [])[-1]
