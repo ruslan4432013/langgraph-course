6ed9f641cf388aad88793e110b0819a9f828e2c4
munir <msunchalyaev@gmail.com>
2026-02-21 02:56:12 +0300

    lesson4 | update description

--- src/agent.py
+++ src/agent.py
@@ -46,7 +46,7 @@ async def call_llm(state: State) -> State:
 
 @tool
 async def get_tasks(name_list: str) -> str:
-    """Просмотреть задачи всех списков через ресурс `to-do://lists/<name_list>'"""
+    """Просмотреть задачи всех списков через ресурс `to-do://tasks'"""
     blobs = await client.get_resources(server_name="to-do-list", uris=[f"to-do://tasks"])
     return [blob.as_string() for blob in blobs][0]
 
@@ -70,9 +70,10 @@ agent = graph.compile()
 
 if __name__ == '__main__':
     result = asyncio.run(
-        agent.ainvoke({"messages": "Пойду в магазин, купить хлеб, молоко и яйца"})
+        agent.ainvoke({"messages": "Пойду в магазин купить хлеб, молоко и яйца"})
     )
+    print(result["messages"][-1].pretty_print())
     result = asyncio.run(
         agent.ainvoke({"messages": "Что нужно сделать в магазине, я забыл"})
     )
-    print(result["messages"][-1].content)
+    print(result["messages"][-1].pretty_print())
